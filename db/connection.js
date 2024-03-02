faf11cfa5007f58dcdffab5b41075ba520d8f826
Ravi <ravikumar87070rk@gmail.com>
2024-03-02 17:47:11 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,6 @@ mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://127.0.0.1/foodOrdering").then(()=> {
     console.log("db has connected successfully ")
 }).catch((err)=> {
-    console.log("error occured")
+    console.log("some error has occured occured")
 });
     
