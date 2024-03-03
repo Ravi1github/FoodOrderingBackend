9ad3b9d22ff3c6caa87689faf21f14b79f075702
Ravi <ravikumar87070rk@gmail.com>
2024-03-03 23:38:08 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,15 @@
 const mongoose=require("mongoose");
 
 mongoose.set('strictQuery', false);                  
-mongoose.connect("mongodb://127.0.0.1/foodOrdering").then(()=> {
-    console.log("db has connected successfully ")
-}).catch((err)=> {
-    console.log("some error has occured occured")
-});
-    
+// mongoose.connect("mongodb://127.0.0.1/foodOrdering").then(()=> {
+//     console.log("db has connected successfully ")
+// }).catch((err)=> {
+//     console.log("error occured")
+// });
+  
+// cloud connect                                
+mongoose.connect('mongodb+srv://ravikumar87070rk:88740rK@cluster0.yokcqlo.mongodb.net/foodorder?retryWrites=true&w=majority&appName=Cluster0').then(()=> {
+    console.log("db has connected successfully with mongoDB ")
+    }).catch((err)=> {
+         console.log("error occured")
+ });
